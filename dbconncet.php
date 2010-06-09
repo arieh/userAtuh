ae1ab476519f0147e3f41a0d1ee8995824e4a6ba
Arieh Glazer <arieh.glazer@gmail.com>
2010-06-09 19:46:48 +0000

    Added changes to readme small fix to dbconnect

--- dbconncet.php
+++ dbconncet.php
@@ -1,7 +1,7 @@
 <?php
 function connect(){
 	$link = mysql_connect("localhost","root","1234");//fill in your database stuff
-	$db = mysql_select_db("hdrc");
+	$db = mysql_select_db("some_db");
 	return $link;
 	//test commitment
 }
